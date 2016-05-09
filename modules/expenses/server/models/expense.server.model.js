63f8a82e50cb56b0a28082303aa5ce1250574c34
Eli Goldberg <lgdragon28@gmail.com>
2016-05-09 23:56:41 +0300

    Removed articles menu and added expenses crud module

---
+++ modules/expenses/server/models/expense.server.model.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+/**
+ * Expense Schema
+ */
+var ExpenseSchema = new Schema({
+  name: {
+    type: String,
+    default: '',
+    required: 'Please fill expense name',
+    trim: true
+  },
+  amount: {
+    type: Number,
+    default: 0,
+    required: 'Please fill expense amount'
+  },
+  picPath: {
+    type: String,
+    default: '',
+  },
+  comments: {
+    type: String,
+    default: '',
+  },
+  category: {
+    type: String,
+    default: '',
+  },
+  isRecurring: {
+    type: Number,
+    default: 0
+
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  },
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  }
+});
+
+mongoose.model('Expense', ExpenseSchema);
